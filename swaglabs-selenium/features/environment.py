dd82e241bb1de76c4d5329c4b6224615797c0f18
Pablo Altamiranda <pabnicalt@gmail.com>
2021-09-15 12:51:14 -0300

    Fixed environment silent errors.

--- swaglabs-selenium/features/environment.py
+++ swaglabs-selenium/features/environment.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import platform
+from resources.exceptions import InvalidBrowser, SystemValueError
 
 
 def before_all(context):
@@ -12,22 +13,28 @@ def before_feature(context, feature):
         if context.system == 'Windows':
             context.driver = webdriver.Chrome(executable_path=r'../drivers/Chrome/windows_chromedriver.exe')
             context.driver.maximize_window()
-        elif context.system == 'Darwin':
+        elif context.system == 'Linux':
             context.driver = webdriver.Chrome(executable_path=r'../drivers/Chrome/linux_chromedriver.exe')
             context.driver.maximize_window()
-        else:
+        elif context.system == 'Darwin':
             context.driver = webdriver.Chrome(executable_path=r'../drivers/Chrome/mac_chromedriver.exe')
+        else:
+            raise SystemValueError('platform.system() returned an invalid value.')
 
-    else:
+    elif context.browser == 'Opera':
         if context.system == 'Windows':
             context.driver = webdriver.Opera(executable_path=r'../drivers/Opera/windows_opera_driver.exe')
             context.driver.maximize_window()
-        elif context.system == 'Darwin':
+        elif context.system == 'Linux':
             context.driver = webdriver.Opera(executable_path=r'../drivers/Opera/linux_opera_driver.exe')
             context.driver.maximize_window()
-        else:
+        elif context.system == 'Darwin':
             context.driver = webdriver.Opera(executable_path=r'../drivers/Opera/mac_opera_driver.exe')
             context.driver.maximize_window()
+        else:
+            raise SystemValueError('platform.system() returned an invalid value.')
+    else:
+        raise InvalidBrowser('Browser is not available or answer given is invalid.')
 
 
 def after_feature(context, feature):
--- swaglabs-selenium/resources/exceptions.py
+++ swaglabs-selenium/resources/exceptions.py
@@ -1,8 +1,8 @@
 class PageValidationError(Exception):
     """Raised when a page can't be validated by 
        the presence of the element it looks for."""
-   
-        
+
+
 class ItemValidationError(Exception):
     """Raised when an item's page doesn't match 
        the item selected in the inventory page."""
@@ -11,3 +11,11 @@ class ItemValidationError(Exception):
 class ItemQuantityError(Exception):
     """Raised when the shopping cart's item
     count does not match the quantity of items added."""
+
+
+class InvalidBrowser(Exception):
+    """Browser is not available or answer given is invalid."""
+
+
+class SystemValueError(Exception):
+    """platform.system() returned an invalid value."""
